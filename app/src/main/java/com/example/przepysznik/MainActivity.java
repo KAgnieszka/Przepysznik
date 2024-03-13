7142b11ae8fbb89e5e331a1d9be9a832f536db20
Maciej Zietek <108173@stud.uz.zgora.pl>
2024-03-13 18:43:56 +0100

    Logowanie

--- app/src/main/java/com/example/przepysznik/MainActivity.java
+++ app/src/main/java/com/example/przepysznik/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.przepysznik.register.LogIn;
 import com.example.przepysznik.register.SignUp;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,8 +13,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sign_up);
-        Intent x = new Intent(MainActivity.this, SignUp.class);
+        setContentView(R.layout.log_in);
+        Intent x = new Intent(MainActivity.this, LogIn.class);
         startActivity(x);
     }
 }
--- app/src/main/java/com/example/przepysznik/register/LogIn.java
+++ app/src/main/java/com/example/przepysznik/register/LogIn.java
@@ -1,5 +1,78 @@
 package com.example.przepysznik.register;
 
-public class LogIn {
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.przepysznik.MainActivity;
+import com.example.przepysznik.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
+public class LogIn extends AppCompatActivity {
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private Button loginButton;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.log_in);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginUser();
+            }
+        });
+    }
+
+    private void loginUser() {
+        String email = emailEditText.getText().toString().trim();
+        String password = passwordEditText.getText().toString().trim();
+
+        if (TextUtils.isEmpty(email) || !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            Toast.makeText(getApplicationContext(), "Podaj poprawny adres email!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(), "Podaj hasło!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Logowanie użytkownika przy użyciu adresu e-mail i hasła
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Logowanie zakończone powodzeniem, przechodzimy do głównej aktywności
+                            Intent intent = new Intent(LogIn.this, MainActivity.class); // Zmiana zależy od nazwy Twojej klasy głównej
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            // Logowanie nie powiodło się
+                            Log.e("Login", "Logowanie nie powiodło się: " + task.getException().getMessage());
+                            Toast.makeText(LogIn.this, "Logowanie nie powiodło się. Spróbuj ponownie.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
 }
