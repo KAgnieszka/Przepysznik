4b3ad178a008d3d7fc177cb386ff8a7fa8ae6778
Jakub Lerka <jakublerka01@gmail.com>
2024-03-16 20:41:48 +0100

    Rozpoczeto prace nad 2 jezykami.
    Dodano obsluge trybu jasnego i ciemnego, dodano przycisk przechodzacy do ustawien systemu do zmiany jezyka systemowego.

--- app/src/main/java/com/example/przepysznik/MainActivity.java
+++ app/src/main/java/com/example/przepysznik/MainActivity.java
@@ -7,13 +7,30 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.przepysznik.register.LogIn;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.mlkit.common.model.DownloadConditions;
+import com.google.mlkit.nl.translate.TranslateLanguage;
+import com.google.mlkit.nl.translate.Translation;
+import com.google.mlkit.nl.translate.Translator;
+import com.google.mlkit.nl.translate.TranslatorOptions;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private Button logout;
-    private Button przycisk;
+    private Button przycisk, settings;
+
+
+    //Link do dokumentacji googla: https://developers.google.com/ml-kit/language/translation/android
+    /*public TranslatorOptions options =
+            new TranslatorOptions.Builder()
+                    .setSourceLanguage(TranslateLanguage.POLISH)
+                    .setTargetLanguage(TranslateLanguage.ENGLISH)
+                    .build();
+    public final Translator polishEnglishTranslator =
+            Translation.getClient(options);
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +38,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         logout = findViewById(R.id.logout);
         przycisk = findViewById(R.id.przycisk);
+        settings = findViewById(R.id.settings);
 
         mAuth = FirebaseAuth.getInstance();
+
+        /*DownloadConditions conditions = new DownloadConditions.Builder()
+                .requireWifi()
+                .build();
+
+        polishEnglishTranslator.downloadModelIfNeeded(conditions)
+                .addOnSuccessListener(
+                        new OnSuccessListener)*/
 
         if (mAuth.getCurrentUser() == null) {
             // Jeśli użytkownik nie jest zalogowany, otwórz ekran logowania
@@ -43,6 +69,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, Share_Recipe.class);
+                startActivity(intent);
+            }
+        });
+
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Settings.class);
                 startActivity(intent);
             }
         });
