b1be11ad6ca272825d03ee6f6537fc5b662aa36e
Agnieszka108145 <108145@g.elearn.uz.zgora.pl>
2024-03-20 21:49:39 +0100

    Logout, nick, e-mail
    Podpiecie wylogowania w menu bocznym, wyswietlanie nicku i emaila w menu po pobraniu z bazy danych

--- app/src/main/java/com/example/przepysznik/MainActivity.java
+++ app/src/main/java/com/example/przepysznik/MainActivity.java
@@ -1,16 +1,29 @@
 package com.example.przepysznik;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.przepysznik.register.LogIn;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.mlkit.common.model.DownloadConditions;
 import com.google.mlkit.nl.translate.TranslateLanguage;
 import com.google.mlkit.nl.translate.Translation;
@@ -20,11 +33,13 @@ import com.google.mlkit.nl.translate.TranslatorOptions;
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
-    private Button logout;
+
+
     private Button przycisk, settings;
 
     // --- Menu boczne --- //
-   // DrawerLayout drawerLayout;
+    DrawerLayout drawerLayout;
+    NavigationView sideMenu;
 
 
     //Link do dokumentacji googla: https://developers.google.com/ml-kit/language/translation/android
@@ -41,11 +56,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        logout = findViewById(R.id.logout);
         przycisk = findViewById(R.id.przycisk);
         settings = findViewById(R.id.settings);
+        sideMenu = findViewById(R.id.side_menu);
 
         mAuth = FirebaseAuth.getInstance();
+
 
         /*DownloadConditions conditions = new DownloadConditions.Builder()
                 .requireWifi()
@@ -60,15 +76,56 @@ public class MainActivity extends AppCompatActivity {
             Intent loginIntent = new Intent(MainActivity.this, LogIn.class);
             startActivity(loginIntent);
         } else {
+            FirebaseUser currentUser = mAuth.getCurrentUser();
+
+            String userId = currentUser.getUid();
+            DatabaseReference usersRef = FirebaseDatabase.getInstance().getReference().child("Users");
+            DatabaseReference currentUserRef = usersRef.child(userId);
+            currentUserRef.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if(snapshot.exists()){
+                        //pobieranie danych z bazy
+                        String nickname = snapshot.child("nickname").getValue(String.class);
+                        String mail = snapshot.child("email").getValue(String.class);
+
+                        // Znajdź element menu w widoku nawigacji
+                        MenuItem profileEmail = sideMenu.getMenu().findItem(R.id.e_mail);
+                        MenuItem profileNick = sideMenu.getMenu().findItem(R.id.ksywka);
+
+                        //wstaw pobrane z bazy dane do menu
+                        profileNick.setTitle(nickname);
+                        profileEmail.setTitle(mail);
+                    }
+
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+
+           // DocumentReference userDocRef = FirebaseFirestore.getInstance().collection("users").document(userId);
+
+           // userDocRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+           //     @Override
+           //     public void onSuccess(DocumentSnapshot documentSnapshot) {
+           //         if (documentSnapshot.exists()) {
+           //             String nickname = documentSnapshot.getString("nickname");
+
+                        // Znajdź element menu w widoku nawigacji
+           //             MenuItem profileInfoItem = sideMenu.getMenu().findItem(R.id.e_mail);
+
+                        // Ustaw ksywkę jako tytuł elementu menu
+           //             profileInfoItem.setTitle("Ksywka: asd" + nickname);
+           //         }
+           //     }
+           // });
+
 
         }
-        logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, LogIn.class);
-                startActivity(intent);
-            }
-        });
 
         przycisk.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -85,6 +142,49 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+        // --- MENU BOCZNE --- //
+        drawerLayout = findViewById(R.id.drawer_layout);
+        sideMenu = findViewById(R.id.side_menu);
+        sideMenu.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if(id == R.id.cookbook)
+                {
+                    Toast.makeText(MainActivity.this, "Wybrano książkę kucharską", Toast.LENGTH_SHORT).show();
+                }
+                if(id == R.id.phoneCall)
+                {
+                    Toast.makeText(MainActivity.this, "Wybrano telefon", Toast.LENGTH_SHORT).show();
+                }
+                if(id == R.id.ustawienia)
+                {
+                    Toast.makeText(MainActivity.this, "Wybrano ustawienia", Toast.LENGTH_SHORT).show();
+                }
+                if(id == R.id.logout)
+                {
+                    Intent intent = new Intent(MainActivity.this, LogIn.class);
+                    startActivity(intent);
+                }
+                if(id == R.id.share)
+                {
+                    Toast.makeText(MainActivity.this, "Wybrano udostępnij", Toast.LENGTH_SHORT).show();
+                }
+                if(id == R.id.about)
+                {
+                    Toast.makeText(MainActivity.this, "Wybrano O nas", Toast.LENGTH_SHORT).show();
+                }
+                if(id == R.id.rate_us)
+                {
+                    Toast.makeText(MainActivity.this, "Wybrano Ocen nas", Toast.LENGTH_SHORT).show();
+                }
+
+                return false;
+            }
+        });
+
     }
 
 }
