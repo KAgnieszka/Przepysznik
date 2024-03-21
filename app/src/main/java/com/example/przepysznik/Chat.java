79575ace734dd973cd0e759674877bfd2f32507b
Jakub Lerka <jakublerka01@gmail.com>
2024-03-21 21:23:05 +0100

    Rewriting MQTT logic

--- app/src/main/java/com/example/przepysznik/Chat.java
+++ app/src/main/java/com/example/przepysznik/Chat.java
@@ -1,5 +1,7 @@
 package com.example.przepysznik;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,37 +16,48 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.przepysznik.register.LogIn;
 
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
 import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+import org.eclipse.paho.android.service.MqttAndroidClient;
 
 
 //Uzyto Ecplise Paho library
 
 public class Chat extends AppCompatActivity {
 
-    private static final String Broker_URL = "tcp://test.mosquitto.org:1883";
-    private static final String Client_ID="client_ID";
-    private MqttHandler mqttHandler;
+    private static final String Broker_URL = "tcp://27df6e50fa3b437fa84505b6f2acd7a0.s1.eu.hivemq.cloud:8883";
+    public static final String Client_ID = "client_ID";
 
+    private MqttAndroidClient client;
     private Button poleczeniePrzycisk, wyslijWiadomosc;
     private TextView polaczenieTest, mqttMessage;
-    private EditText wiadomoscText;
+    public EditText wiadomoscText;
+    public String messageText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chat);
-        mqttHandler = new MqttHandler();
         poleczeniePrzycisk = findViewById(R.id.poleczeniePrzycisk);
         wyslijWiadomosc = findViewById(R.id.wyslijWiadomosc);
         polaczenieTest = findViewById(R.id.connectionText);
         wiadomoscText = findViewById(R.id.wiadomoscText);
         mqttMessage = findViewById(R.id.mqttMessage);
 
+        client = new MqttAndroidClient(this.getApplicationContext(), Broker_URL, Client_ID);
+
         poleczeniePrzycisk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mqttHandler.connect(Broker_URL, Client_ID);
+                connect();
                 polaczenieTest.setText("Połączono z MQTT");
             }
         });
@@ -52,38 +65,84 @@ public class Chat extends AppCompatActivity {
         wyslijWiadomosc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String messageText = wiadomoscText.getText().toString();
+                messageText = wiadomoscText.getText().toString();
                 publishMessage("Test", messageText);
-                subscribeToTopic("Test");
-
-                //messageArrived();
             }
         });
     }
 
     @Override
     protected void onDestroy() {
-        mqttHandler.disconnect();
+        disconnect();
         super.onDestroy();
     }
 
     private void publishMessage(String topic, String message) {
         Toast.makeText(this, "Publishing message: " + message, Toast.LENGTH_SHORT).show();
-        mqttHandler.publish(topic, message);
+        publish(topic, message);
     }
 
-    private void subscribeToTopic(String topic){
-        Toast.makeText(this, "Subscribing to topic: " + topic, Toast.LENGTH_SHORT).show();
-        mqttHandler.subscribe(topic);
-        MqttCallback(String topic, MqttMessage message) {
+    public void connect() {
+        try {
+            //MemoryPersistence persistence = new MemoryPersistence();
+            IMqttToken token = client.connect();
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    Log.d(TAG, "Connected!");
+                    subscribe("Test");
+                }
 
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Log.d(TAG, "Not connected!");
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
         }
     }
 
-    public void messageArrived(String topic, MqttMessage message) throws Exception {
-        Log.d("tag","message>>" + new String(message.getPayload()));
-        Log.d("tag","topic>>" + topic);
-        mqttMessage.setText(new String(message.getPayload()));
+    public void disconnect() {
+        try {
+            client.disconnect();
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
 
+    public void publish(String topic, String message) {
+        try {
+            MqttMessage mqttMessage = new MqttMessage(message.getBytes());
+            client.publish(topic, mqttMessage);
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void subscribe(String topic) {
+        try {
+            client.subscribe(topic, 0);
+            client.setCallback(new MqttCallback() {
+                @Override
+                public void connectionLost(Throwable cause) {
+                    Log.d(TAG, "connectionLost");
+                }
+
+                @Override
+                public void messageArrived(String topic, MqttMessage message) throws Exception {
+                    Log.d(TAG, "Topic: " + topic + "\n");
+                    Log.d(TAG, "message: " + new String(message.getPayload()));
+                    mqttMessage.setText(new String(message.getPayload()));
+                }
+
+                @Override
+                public void deliveryComplete(IMqttDeliveryToken token) {
+                    //Toast.makeText(this, "Messaged delivered: " + topic, Toast.LENGTH_SHORT).show();
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/example/przepysznik/MqttHandler.java
+++ app/src/main/java/com/example/przepysznik/MqttHandler.java
@@ -1,10 +1,18 @@
 package com.example.przepysznik;
 
+import static android.content.ContentValues.TAG;
+
+import android.util.Log;
+import android.widget.Toast;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
 
 public class MqttHandler {
 
@@ -49,6 +57,23 @@ public class MqttHandler {
     public void subscribe(String topic) {
         try {
             client.subscribe(topic);
+            client.setCallback(new MqttCallback() {
+                @Override
+                public void connectionLost(Throwable cause) {
+                    Log.d(TAG, "connectionLost");
+                }
+
+                @Override
+                public void messageArrived(String topic, MqttMessage message) throws Exception {
+                    Log.d(TAG, "Topic: "+ topic+"\n");
+                    Log.d(TAG, "message: "+ new String(message.getPayload()));
+                }
+
+                @Override
+                public void deliveryComplete(IMqttDeliveryToken token) {
+                    //Toast.makeText(this, "Messaged delivered: " + topic, Toast.LENGTH_SHORT).show();
+                }
+            });
         } catch (MqttException e) {
             e.printStackTrace();
         }
