9e587cce01f88f180d7b97d88212ca850bc31845
Agnieszka108145 <108145@g.elearn.uz.zgora.pl>
2024-04-02 21:29:16 +0200

    ActionBar do menu bocznego i themy
    kolor themu zmienia sie w colors.xml

--- app/src/main/java/com/example/przepysznik/MainActivity.java
+++ app/src/main/java/com/example/przepysznik/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.przepysznik;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
@@ -50,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
     // --- Menu boczne --- //
     DrawerLayout drawerLayout;
     NavigationView sideMenu;
+    ActionBarDrawerToggle drawerToggle;
 
 
     //Link do dokumentacji googla: https://developers.google.com/ml-kit/language/translation/android
@@ -67,8 +70,17 @@ public class MainActivity extends AppCompatActivity {
     private Button adBtn;
 
     @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(drawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //setTheme(R.style.AppTheme); // Ustawienie tematu z ActionBar
         setContentView(R.layout.activity_main);
         przycisk = findViewById(R.id.przycisk);
         chat = findViewById(R.id.chat);
@@ -165,6 +177,10 @@ public class MainActivity extends AppCompatActivity {
         // --- MENU BOCZNE --- //
         drawerLayout = findViewById(R.id.drawer_layout);
         sideMenu = findViewById(R.id.side_menu);
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         sideMenu.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -250,4 +266,15 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public void onBackPressed()
+        {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else
+        {
+            super.onBackPressed();
+        }
+    }
 }
